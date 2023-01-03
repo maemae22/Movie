7cd55c6a7ccd7e7c0280be7d2274f54d183b8363
maemae22 <maemae2@naver.com>
2023-01-03 21:06:58 +0900

    #18 - PageController 수정

--- src/main/java/com/example/movie/page/controller/PageController.java
+++ src/main/java/com/example/movie/page/controller/PageController.java
@@ -2,6 +2,7 @@ package com.example.movie.page.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
@@ -11,5 +12,14 @@ public class PageController {
         return "index";
     }
 
+    @GetMapping("/movie")
+    public String movieList() {
+        return "movie";
+    }
+
+    @GetMapping("/movie_detail/{movieId}")
+    public String movieDetail(@PathVariable long movieId) {
+        return "movie_detail";
+    }
 
 }

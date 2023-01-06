bdfe3deda5d7c34c9962985f1e13eaa41e8fc937
maemae22 <maemae2@naver.com>
2023-01-06 19:37:01 +0900

    #32 - MovieController 변경 및 수정

--- src/main/java/com/example/movie/controller/MovieController.java
+++ src/main/java/com/example/movie/controller/MovieController.java
@@ -4,6 +4,7 @@ import com.example.movie.dto.MovieDTO;
 import com.example.movie.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +15,14 @@ public class MovieController {
     @Autowired
     MovieService ms;
 
-    @GetMapping("/movies")
-    public List<MovieDTO> selectAllMovie() {
-        return ms.selectAllMovie();
+    @GetMapping("/api/movies")
+    public List<MovieDTO> selectAllMovies() {
+        return ms.selectAllMovies();
+    }
+
+    @GetMapping("/api/movie/{movieCd}")
+    public MovieDTO selectMovieDetailByMovieCode(@PathVariable int movieCd) {
+        return ms.selectMovieDetailByMovieCode(movieCd);
     }
 }
 

e454dcf4b443027b5419d3aac6074c852ba3679f
maemae22 <maemae2@naver.com>
2023-01-06 19:36:34 +0900

    #32 - CommentController 변경 및 수정

--- src/main/java/com/example/movie/controller/CommentController.java
+++ src/main/java/com/example/movie/controller/CommentController.java
@@ -1,22 +1,37 @@
 package com.example.movie.controller;
 
+import com.example.movie.dto.CommentDTO;
+import com.example.movie.dto.MovieDTO;
+import com.example.movie.login.service.LoginService;
 import com.example.movie.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class CommentController {
 
     @Autowired
     CommentService cs;
+    @Autowired
+    LoginService ls;
 
-    @GetMapping("movie/{movieId}")
-    public String movieDetail(@PathVariable long movieId, Model model) {
-        return "movie_detail";
+    @PostMapping("/api/comment/add")
+    public int insertComment(CommentDTO commentDTO, HttpSession session) {
+        String nickname = String.valueOf(session.getAttribute("nickname"));
+        commentDTO.setMemberId(ls.selectMemberIdByEmail(nickname));
+        return cs.insertComment(commentDTO);
     }
+
+    @GetMapping("/api/comments/{movieCd}")
+    public List<CommentDTO> selectAllCommentByMovieId(@PathVariable int movieCd) {
+        return cs.selectAllCommentByMovieId(movieCd);
+    }
+
 
 }

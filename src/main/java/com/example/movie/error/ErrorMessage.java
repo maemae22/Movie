f962dd48646ad63f0509082d91dc9bf68894a827
codingvegeta <hong090992@gmail.com>
2023-01-12 05:11:05 +0900

    #45 유효성 검증
    프론트에서 넘어오는 값의 기본 유효성 검증하는 멤버 DTO로 변경하였습니다.
    중복 에 대한 에러메세지를 생성하였습니다.
    회원가입 페이지를 수정하였습니다.

---
+++ src/main/java/com/example/movie/error/ErrorMessage.java
@@ -0,0 +1,8 @@
+package com.example.movie.error;
+
+public class ErrorMessage {
+
+        public static final String ERR_MSG_DUPLICATED_ID = "중복된 유저 ID가 있습니다. 다른 ID를 입력해 주세요";
+        public static final String ERR_MSG_DUPLICATED_NICKNAME = "중복된 유저 Nickname이 있습니다. 다른 Nickname을 입력해 주세요.";
+
+    }

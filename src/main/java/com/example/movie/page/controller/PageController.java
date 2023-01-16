d9c6257c4787f5c89f963da66363d90f2363aa56
maemae22 <maemae2@naver.com>
2023-01-16 17:18:10 +0900

    #55 - PageController 수정 (movie.html -> movies.html)
    movies.html 로 가도록 PageController 수정함

--- src/main/java/com/example/movie/page/controller/PageController.java
+++ src/main/java/com/example/movie/page/controller/PageController.java
@@ -32,7 +32,7 @@ public class PageController {
 
     @GetMapping("/movie")
     public String movieList() {
-        return "movie";
+        return "movies";
     }
 
     @GetMapping("/movie/{movieCode}")

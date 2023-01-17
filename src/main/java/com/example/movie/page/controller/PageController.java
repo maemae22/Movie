1f0c7ecc42d59ad0838c3980515b9f973939efcb
maemae22 <maemae2@naver.com>
2023-01-17 20:55:53 +0900

    #57 - PageController에 Authentication, Model 추가
    로그인 유저의 권한을 받아와 Model 로 전달함

--- src/main/java/com/example/movie/page/controller/PageController.java
+++ src/main/java/com/example/movie/page/controller/PageController.java
@@ -1,8 +1,12 @@
 package com.example.movie.page.controller;
 
+import com.example.movie.dto.MemberDTO;
+import com.example.movie.service.MemberService;
 import com.example.movie.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +20,9 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 public class PageController {
     private final MovieService ms;
+
+    @Autowired
+    MemberService memberService;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -36,8 +43,14 @@ public class PageController {
     }
 
     @GetMapping("/movie/{movieCode}")
-    public String movieDetail(@PathVariable int movieCode, Model model) {
+    public String movieDetail(@PathVariable int movieCode, Model model, Authentication authentication) {
         model.addAttribute("movieCode", movieCode);
+
+        MemberDTO memberDTO = memberService.selectMemberDetail(authentication.getName());
+        model.addAttribute("memberDTO", memberDTO);
+        model.addAttribute("memberId", memberDTO.getId());
+        model.addAttribute("nickname", memberDTO.getNickname());
+
         return "movie_detail";
     }
 
